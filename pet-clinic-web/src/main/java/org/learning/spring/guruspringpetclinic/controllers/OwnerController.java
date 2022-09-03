cb143b8ad6d10852e0822a3ba0c84e3d4f0d592d
morenosh <morenoshadravan@gmail.com>
2022-09-03 17:53:22 +0430

    create index page of owner and controller. closes #12

---
+++ pet-clinic-web/src/main/java/org/learning/spring/guruspringpetclinic/controllers/OwnerController.java
@@ -0,0 +1,14 @@
+package org.learning.spring.guruspringpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listOwners(){
+        return "owners/index.html";
+    }
+}
