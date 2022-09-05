59da5418c0d309d6edefc667237c6fb3fd7341a7
morenosh <morenoshadravan@gmail.com>
2022-09-05 14:06:11 +0430

    vet controller list all vets. closes #14

--- pet-clinic-web/src/main/java/org/learning/spring/guruspringpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/org/learning/spring/guruspringpetclinic/controllers/VetController.java
@@ -1,14 +1,25 @@
 package org.learning.spring.guruspringpetclinic.controllers;
 
+import org.learning.spring.guruspringpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/vets")
 @Controller
 public class VetController {
 
-    @RequestMapping({"","/", "/index", "/index.html"})
-    public String listVets(){
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
+        
         return "vets/index.html";
+
     }
 }
