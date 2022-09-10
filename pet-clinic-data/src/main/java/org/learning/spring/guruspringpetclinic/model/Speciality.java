d6cc6a4ebfd5dbd7a4ee66152f19705bad4eadd4
morenosh <morenoshadravan@gmail.com>
2022-09-10 20:48:26 +0430

    add Speciality entity and add set of specialities in vet entity. closes #25.

---
+++ pet-clinic-data/src/main/java/org/learning/spring/guruspringpetclinic/model/Speciality.java
@@ -0,0 +1,13 @@
+package org.learning.spring.guruspringpetclinic.model;
+
+public class Speciality extends Person {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
