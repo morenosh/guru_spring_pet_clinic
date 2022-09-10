b0a3e31001ff9a5b5d8b77ea2d7a54188dabe3c3
morenosh <morenoshadravan@gmail.com>
2022-09-10 20:43:51 +0430

    add getter/setter for Pet entity| add set of pets for Owner| add Visit entity. closes #24.

--- pet-clinic-data/src/main/java/org/learning/spring/guruspringpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/org/learning/spring/guruspringpetclinic/model/Owner.java
@@ -1,4 +1,15 @@
 package org.learning.spring.guruspringpetclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person{
+    private Set<Pet> pet;
+
+    public Set<Pet> getPet() {
+        return pet;
+    }
+
+    public void setPet(Set<Pet> pet) {
+        this.pet = pet;
+    }
 }
--- pet-clinic-data/src/main/java/org/learning/spring/guruspringpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/org/learning/spring/guruspringpetclinic/model/Pet.java
@@ -2,8 +2,32 @@ package org.learning.spring.guruspringpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet extends BaseEntity{
+public class Pet extends BaseEntity {
     private PetType type;
-    private LocalDate birthDate;
     private Owner owner;
+    private LocalDate birthDate;
+
+    public PetType getType() {
+        return type;
+    }
+
+    public void setType(PetType type) {
+        this.type = type;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
 }
