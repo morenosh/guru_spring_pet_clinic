77a9ae8c85e2dd52fdb86ec5acb9c379014afa53
morenosh <morenoshadravan@gmail.com>
2022-09-12 11:07:51 +0430

    implement speciality map service. closes #29.

--- pet-clinic-data/src/main/java/org/learning/spring/guruspringpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/org/learning/spring/guruspringpetclinic/model/Vet.java
@@ -1,9 +1,10 @@
 package org.learning.spring.guruspringpetclinic.model;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class Vet extends Person{
-    private Set<Speciality> specialities;
+    private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
         return specialities;
---
+++ pet-clinic-data/src/main/java/org/learning/spring/guruspringpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package org.learning.spring.guruspringpetclinic.services;
+
+import org.learning.spring.guruspringpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+}
