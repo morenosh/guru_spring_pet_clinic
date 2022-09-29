c987e12ac647cf5d3ff77b05d26a098cf72093e3
morenosh <morenoshadravan@gmail.com>
2022-09-29 18:46:17 +0330

    update vet & specialities with jpa annotation. closes #35.

--- pet-clinic-data/src/main/java/org/learning/spring/guruspringpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/org/learning/spring/guruspringpetclinic/model/Vet.java
@@ -1,9 +1,15 @@
 package org.learning.spring.guruspringpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
-public class Vet extends Person{
+@Entity
+@Table(name = "vets")
+public class Vet extends Person {
+    @ManyToMany
+    @JoinTable(name = "vet_specialities", joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
