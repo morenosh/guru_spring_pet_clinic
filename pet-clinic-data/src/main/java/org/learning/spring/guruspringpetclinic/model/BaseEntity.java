3d1969e2473c25c28a849717ba3f6e653c8e33aa
morenosh <morenoshadravan@gmail.com>
2022-09-29 18:26:53 +0330

    update base entity with jpa annotation. closes #33.

--- pet-clinic-data/src/main/java/org/learning/spring/guruspringpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/org/learning/spring/guruspringpetclinic/model/BaseEntity.java
@@ -1,8 +1,16 @@
 package org.learning.spring.guruspringpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
     public Long getId() {
