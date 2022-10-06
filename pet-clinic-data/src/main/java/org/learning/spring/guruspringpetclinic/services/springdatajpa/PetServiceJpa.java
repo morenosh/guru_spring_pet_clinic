e78ef6cc211ba48994ea54a165cafe886e27555e
morenosh <morenoshadravan@gmail.com>
2022-10-06 14:03:40 +0330

    add and implement spring data jpa service for pet. Closes #40.

---
+++ pet-clinic-data/src/main/java/org/learning/spring/guruspringpetclinic/services/springdatajpa/PetServiceJpa.java
@@ -0,0 +1,49 @@
+package org.learning.spring.guruspringpetclinic.services.springdatajpa;
+
+import org.learning.spring.guruspringpetclinic.model.Pet;
+import org.learning.spring.guruspringpetclinic.model.Vet;
+import org.learning.spring.guruspringpetclinic.repositories.PetRepository;
+import org.learning.spring.guruspringpetclinic.services.PetService;
+import org.springframework.data.util.Streamable;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetServiceJpa implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetServiceJpa(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        return Streamable.of(petRepository.findAll()).toSet();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return petRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        if (object == null) throw new RuntimeException("argument can not be null");
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        if (object == null) throw new RuntimeException("argument can not be null");
+        if (object.getId() == null) throw new RuntimeException("id of entity to be deleted can not be null");
+
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petRepository.deleteById(id);
+    }
+}
